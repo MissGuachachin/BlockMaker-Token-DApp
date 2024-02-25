2e3d58ae992f9c40d35d2cf18d8533f6bebac84e
Maggie <guachachi02@yahoo.es>
2024-02-26 00:35:28 +0100

    Añadir y estructurar Padre e Hijo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <div className="bg-blue-200 flex justify-between">
+        <div className="bg-red-100 w-fit">Soy el hijo</div>
+        <div className="bg-red-100 w-fit">Soy el hijo</div>
+        <div className="bg-red-100 w-fit">Soy el hijo</div>
+      </div>
+    </h1>
+  )
 }
 
 export default App
