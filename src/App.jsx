05b396e01b838f72bd5281fc90b9e8b9a9392f05
Maggie <guachachi02@yahoo.es>
2024-02-26 04:09:09 +0100

    Actualizar componente App añadiendo pagina Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { AppLayout } from './components/UI/Layouts'
+import { Home } from './pages'
 
 function App() {
   return (
     <AppLayout>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home />
     </AppLayout>
   )
 }
