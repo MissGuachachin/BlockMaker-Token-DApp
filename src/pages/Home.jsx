0cefd1f4af8370343eb2047b7ce98fcbac4503fa
Maggie <guachachi02@yahoo.es>
2024-02-26 04:07:00 +0100

    Añadir componente pagina Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,2 +1,7 @@
-export { default as Home } from './Home';
-
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    )
+}
